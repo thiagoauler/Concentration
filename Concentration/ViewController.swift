2add94bdb4311c726eed5c8d943846f9301bbeb2
Thiago Auler dos Santos <thiagoauler@gmail.com>
2018-02-10 12:19:11 -0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Thiago Auler dos Santos on 10/02/2018.
+//  Copyright © 2018 Thiago Auler dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
